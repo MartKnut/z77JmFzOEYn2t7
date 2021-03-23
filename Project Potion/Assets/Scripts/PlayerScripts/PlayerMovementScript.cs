560aca7ea0426de9300cdb1be3e06d2ea589f1c1
Martin <martin.knutsen@live.no>
2021-03-23 20:03:15 +0100

    InitialProject

---
+++ Project Potion/Assets/Scripts/PlayerScripts/PlayerMovementScript.cs
@@ -0,0 +1,52 @@
+﻿using SoundScripts;
+using UnityEngine;
+
+namespace PlayerScripts
+{
+    public class PlayerMovementScript : MonoBehaviour
+    {
+        private Animator anim;
+            // We want to get this gameObject's Rigidbody2D, let's call it "rb"
+        private Rigidbody2D rb;
+
+        // The speed we want our gameObject to move. (The Player)
+        public float moveSpeed;
+        
+        
+        // Start is called before the first frame update
+        void Start()
+        {
+            // rb is equal to this gameObject's component Rigidbody2D
+            rb = gameObject.GetComponent<Rigidbody2D>();
+            // Writing this defeats the purpose of having rb public, and is more effective for the engine.
+
+            anim = gameObject.GetComponent<Animator>();
+        }
+        
+        // Update is called once per frame
+        void Update()
+        {
+            Movement();
+        }
+
+        private void Movement()
+        {
+            FindObjectOfType<AudioManager>().Play("Walk");
+            
+            
+            // Gets the Horizontal X value, and Vertical Y value. WS,AD or Arrow Keys in this case.
+            var x = Input.GetAxisRaw("Horizontal");
+            var y = Input.GetAxisRaw("Vertical");
+
+            // We want to give our rigidbody rb a velocity of (+-1 * Movespeed). GetAxisRaw gives us a value of either 1 or 0.
+            // This gives the player snappy and satisfying controlling, and not floaty like GetAxis
+            rb.velocity = new Vector2(x, y).normalized * moveSpeed;
+            
+        }
+
+        private void AnimValue()
+        {
+            
+        }
+    }
+}
---
+++ Project Potion/Assets/Scripts/SoundScripts/AudioManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using UnityEngine;
+
+namespace SoundScripts
+{
+    public class AudioManager : MonoBehaviour
+    {
+    
+        public static AudioManager Instance;
+    
+        public Sound[] sounds;
+
+        private void Awake()
+        {
+        
+            if (Instance == null) Instance = this;
+            else
+            {
+                Destroy(gameObject);
+                return;
+            }
+        
+            foreach (Sound s in sounds)
+            {
+                s.source = gameObject.AddComponent<AudioSource>();
+                s.source.clip = s.clip;
+
+                s.source.volume = s.volume;
+                s.source.pitch = s.pitch;
+            }
+        }
+
+    
+
+        public void Play(string soundName)
+        {
+            Sound s = Array.Find(sounds, sound => sound.name == soundName);
+            s.source.Play();
+        }
+    }
+}
